2848df8c5714065b19d573859188a5e907a8af9e
Alex Stroulger <alexstroulger@gmail.com>
2018-01-31 10:04:00 -0500

    Fixes to work with new version of mongodb

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) return console.log(err)
-  database = database.db("notes")
+
+  // Add this line below to make it work with the new version of mongodb
+  // (make sure you add the database name and not the collection name)
+
+  database = database.db("canada-weather-api")
+
   require("./app/routes")(app, database)
   app.listen(port, () => {
     console.log("we are live on " + port)
